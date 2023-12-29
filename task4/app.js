572f3331113fa965db057fee1de6ab44cdadf5b2
aygun aghalarova <agalarova.aygunn@gmail.com>
2023-12-29 23:10:25 +0400

    task4 is ready

---
+++ task4/app.js
@@ -0,0 +1,30 @@
+
+// var number = prompt("1 ədəd daxil edin");
+// if (number / 10000 >= 1) {
+//   stPer = number * 0.18;
+//   ndPer = stPer * 0.03;
+//   console.log("Ədədin 18%-i " + stPer);
+//   console.log("Ədədin 3%-i " + ndPer);
+// } else {
+//   console.log("eded 5 reqemli deyil");
+// }
+
+
+
+// var number = prompt("ədəd daxil edin");
+// var count = 0;
+// var n;
+
+// while (number >= 5) {
+//   n = number % 10;
+//   number = Math.floor(number / 10);
+//   if (n == 5) {
+//     count++;
+//     break;
+//   }
+// }
+// if (count == 1) {
+//   console.log("bəli");
+// } else {
+//   console.log("xeyr");
+// }
